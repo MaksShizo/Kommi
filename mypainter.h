4fb88e2e722815059fe8aaa3d2f9582ac3f997ba
MaksShizo <79019923+MaksShizo@users.noreply.github.com>
2021-05-23 20:59:10 +0500

    Add files via upload

---
+++ mypainter.h
@@ -0,0 +1,23 @@
+#ifndef MYPAINTER_H
+#define MYPAINTER_H
+
+#include <fstream>
+#include <QBrush>
+#include <QFont>
+#include <QPen>
+#include <QWidget>
+
+class myPainter
+{
+public:
+    myPainter();
+public:
+    void draw(QPainter *painter, QPaintEvent *event);
+    QBrush front;
+private:
+    QBrush back;
+    QFont glFont;
+    QPen textPen;
+};
+
+#endif //MYPAINTER_H
